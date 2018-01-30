4766a16fc8db9487c052f3902bf43f2885f5975d
Conroy <itsconroy+github@gmail.com>
2018-01-30 23:36:42 +0100

    some css and height caluclation improvements
    still not perfect as it relies on timeout

--- src/components/threadlist.vue
+++ src/components/threadlist.vue
@@ -3,9 +3,9 @@
     <div class="box-shadow-wrapper">
       <h4 class="title is-size-4 headline">Active threads on /{{ selectedBoard }}/</h4>
       <div class="threads-wrapper" v-if="selectedBoard">
-        <a v-for="(thread,index) in threadData[selectedBoard].slice(0,threadsToShow)" :key="thread.no" :href="`https://boards.4chan.org/${selectedBoard}/thread/${thread.no}`" target="_blank" rel="noopener">
+        <a v-for="thread in threadData[selectedBoard].slice(0,threadsToShow)" :key="thread.no" :href="`https://boards.4chan.org/${selectedBoard}/thread/${thread.no}`" target="_blank" rel="noopener">
           <img :src="thread.image" referrerpolicy="same-origin" ref="img" alt="404 desu senpai" @error="thread.image='https://s.4cdn.org/image/error/404/404-DanKim.gif'">
-          <div class="text-wrapper has-text-left ">
+          <div class="text-wrapper">
             <div class="thread-ppm" v-if="thread.replies == 9001">
               {{ thread.postsPerMinute.toFixed(2) }} posts/min - <u>Sticky Thread</u> - {{ (thread.age / (1000 * 60 * 60)).toFixed(1) }} hours ago
             </div>
@@ -40,18 +40,15 @@ export default {
 	},
 	methods: {
 		setListHeight(){
-			setTimeout(() => {
-				this.$nextTick(function () {
-					let boardsWrapper = document.querySelector(".boardlist-wrapper")
-					//console.log("ele1",boardsWrapper)
-					this.listHeight = boardsWrapper.clientHeight
-					this.threadsToShow = localStorage.getItem("forceActiveThreadCount") || Math.max(Math.floor(this.listHeight / 150),4)
-					//console.log(this.threadsToShow)
-					let threadsWrapper = document.querySelector(".threads-wrapper")
-					//console.log("ele2",threadsWrapper)
-					threadsWrapper.style.minHeight = this.listHeight + "px"
-				})
-			},500)
+			pino.debug("setListHeight")
+			let boardsWrapper = document.querySelector(".boardlist-wrapper")
+			//console.log("ele1",boardsWrapper)
+			this.listHeight = boardsWrapper.clientHeight
+			this.threadsToShow = localStorage.getItem("forceActiveThreadCount") || Math.max(Math.floor(this.listHeight / 150),4)
+			//console.log(this.threadsToShow)
+			let threadsWrapper = document.querySelector(".threads-wrapper")
+			//console.log("ele2",threadsWrapper)
+			threadsWrapper.style.minHeight = this.listHeight + "px"
 		},
 		revealThreadSideBar(doScrollToTop){
 			pino.debug("revealThreadSideBar")
@@ -76,7 +73,7 @@ export default {
 		this.setListHeight()
 		this.$store.subscribe(mutation => {
 			if(mutation.type == "setEnabledBoards" || mutation.type == "setInitialData"){
-				this.setListHeight()
+				setTimeout(this.setListHeight,2000) //FIXME: not very elegant
 			}
 		})
     
@@ -185,7 +182,27 @@ img {
   line-height: 1;
   position: relative;
 
-  .overlay{
+  &>.thread-ppm {
+    padding: 0.25rem;
+    font-size: 0.75rem;
+    color: $nord0;
+    margin: 0;
+  }
+
+  &>.thread-title {
+    padding: 0.25rem;
+    font-size: 0.75rem;
+    font-weight: bolder;
+    margin: 0;
+    white-space: nowrap;
+  }
+
+  &>.thread-comment {
+    padding: 0.25rem;
+    font-size: 0.75rem;
+  }
+
+  &>.overlay{
     position: absolute;
     top: 0;
     left: 0;
@@ -195,25 +212,7 @@ img {
   }
 }
 
-.thread-ppm {
-  padding: 0.25rem;
-  font-size: 0.75rem;
-  color: $nord0;
-  margin: 0;
-}
 
-.thread-title {
-  padding: 0.25rem;
-  font-size: 1rem;
-  font-weight: bolder;
-  margin: 0;
-  white-space: nowrap;
-}
-
-.thread-comment {
-  padding: 0.25rem;
-  font-size: 0.75rem;
-}
 
 @keyframes swipeHint {
   0% {
