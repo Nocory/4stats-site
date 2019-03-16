57ac83a876ae86847aa7d5ece9a91d92c69fdcb3
Conroy <itsconroy+github@gmail.com>
2019-03-16 19:40:09 +0100

    missing sideimage

---
+++ src/components/sideImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sideImage-component" v-once>
+    <img src="~/static/Rockwell_TheKansasCitySpirit_small.jpg">
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.sideImage-component{
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  @include float-shadow-box;
+}
+img{
+  width: 100%;
+  object-fit: contain;
+}
+</style>
