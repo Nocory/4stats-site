455a44d542ccfc6c1acf4682f6844c0e223e50af
Nocory <itsconroy+github@gmail.com>
2019-07-26 20:37:13 +0200

    update side image

--- src/components/sideImage.vue
+++ src/components/sideImage.vue
@@ -1,18 +1,18 @@
 <template>
-	<div class="sideImage-component" v-once>
-		<img src="~/static/1543041166611cc.jpg" />
-	</div>
+  <div v-once class="sideImage-component">
+    <img src="~/static/h.png" />
+  </div>
 </template>
 
 <style scoped lang="scss">
 .sideImage-component {
-	display: flex;
-	flex-direction: column;
-	position: relative;
-	@include float-shadow-box;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  @include float-shadow-box;
 }
 img {
-	width: 100%;
-	object-fit: contain;
+  width: 100%;
+  object-fit: contain;
 }
 </style>
