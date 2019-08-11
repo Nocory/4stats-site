cfb3f021050abdf169f5bab705b0d7b55a30aa42
Nocory <itsconroy+github@gmail.com>
2019-08-11 20:11:18 +0200

    swapped sideimage with info component

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,62 +1,59 @@
 <template>
-	<div class="meta-component" v-once>
-		<div class="component-title">Info</div>
-		<div class="component-content">
-			<p>
-				Analyzed 4 weeks of posts and calculated the words, that appear more frequently on each board compared to the site average.<br />
-				<br />
-				<a href="https://pastebin.com/raw/bxBZrzB5">https://pastebin.com/raw/bxBZrzB5</a><br />
-				(Event further improved. Should be fairly accurate now.)
-			</p>
-			<!--<img src="~/static/1543041166611.jpg" />-->
-		</div>
-	</div>
+  <div v-once class="meta-component">
+    <div class="component-title">Info</div>
+    <div class="component-content">
+      <p>
+        Have a nice day
+      </p>
+      <!--<img src="~/static/1543041166611.jpg" />-->
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .meta-component {
-	display: flex;
-	flex-direction: column;
-	@include float-shadow-box;
-	position: relative;
-	//height: 100px;
+  display: flex;
+  flex-direction: column;
+  @include float-shadow-box;
+  position: relative;
+  //height: 100px;
 }
 
 .component-content {
-	//background: $--background-content-2n;
-	background: var(--background-content);
-	display: flex;
-	flex-direction: column;
-	padding: 1em;
-	color: var(--color-text);
+  //background: $--background-content-2n;
+  background: var(--background-content);
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  color: var(--color-text);
 
-	line-height: 1.2;
-	//font-size: 0.8em;
+  line-height: 1.2;
+  //font-size: 0.8em;
 }
 
 b {
-	text-decoration: underline;
+  text-decoration: underline;
 }
 
 hr {
-	background: var(--color-text);
-	margin: 0.5rem 0;
-	width: 10%;
+  background: var(--color-text);
+  margin: 0.5rem 0;
+  width: 10%;
 }
 
 p {
-	margin: 0;
-	&:not(:last-child) {
-		margin: 0 0 1rem;
-	}
+  margin: 0;
+  &:not(:last-child) {
+    margin: 0 0 1rem;
+  }
 }
 
 img {
-	width: 100%;
-	object-fit: contain;
+  width: 100%;
+  object-fit: contain;
 }
 
 a {
-	text-decoration: underline;
+  text-decoration: underline;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="index-component">
-		<component-boardlist />
-		<component-threadlist />
-		<div v-if="renderRight" class="right is-hidden-below-widescreen">
-			<div class="right-wrapper">
-				<component-meta />
-				<!--<component-info />-->
-				<component-sideImage />
-			</div>
-			<img class="really-makes-you-think-doesnt-it is-hidden-touch" src="~/thunk.png" />
-		</div>
-		<component-chart v-if="renderChart" class="is-hidden-below-tablet" />
-	</div>
+  <div class="index-component">
+    <component-boardlist />
+    <component-threadlist />
+    <div v-if="renderRight" class="right is-hidden-below-widescreen">
+      <div class="right-wrapper">
+        <component-meta />
+        <component-info />
+        <!--<component-sideImage />-->
+      </div>
+      <img class="really-makes-you-think-doesnt-it is-hidden-touch" src="~/thunk.png" />
+    </div>
+    <component-chart v-if="renderChart" class="is-hidden-below-tablet" />
+  </div>
 </template>
 
 <script>
@@ -19,115 +19,115 @@ import componentBoardlist from "components/boardlist.vue"
 import componentThreadlist from "components/threadlist.vue"
 //import componentSideImage from "components/sideImage.vue"
 export default {
-	data: () => ({
-		renderChart: window.innerWidth >= 768,
-		renderRight: window.innerWidth >= 1224
-	}),
-	components: {
-		componentBoardlist,
-		componentThreadlist,
-		componentMeta: () => import(/* webpackChunkName: "sidebar" */ "components/meta.vue"),
-		componentInfo: () => import(/* webpackChunkName: "sidebar" */ "components/info.vue"),
-		componentSideImage: () => import(/* webpackChunkName: "sidebar" */ "components/sideImage.vue"),
-		componentChart: () => import(/* webpackChunkName: "chart" */ "components/chart.vue")
-	},
-	mounted() {
-		window.addEventListener("resize", this.documentResize, {
-			passive: true
-		})
-	},
-	beforeDestroy() {
-		window.removeEventListener("resize", this.documentResize, {
-			passive: true
-		})
-	},
-	methods: {
-		documentResize() {
-			this.renderChart = window.innerWidth >= 768
-			this.renderRight = window.innerWidth >= 1224
-		}
-	}
+  components: {
+    componentBoardlist,
+    componentThreadlist,
+    componentMeta: () => import(/* webpackChunkName: "sidebar" */ "components/meta.vue"),
+    componentInfo: () => import(/* webpackChunkName: "sidebar" */ "components/info.vue"),
+    //componentSideImage: () => import(/* webpackChunkName: "sidebar" */ "components/sideImage.vue"),
+    componentChart: () => import(/* webpackChunkName: "chart" */ "components/chart.vue")
+  },
+  data: () => ({
+    renderChart: window.innerWidth >= 768,
+    renderRight: window.innerWidth >= 1224
+  }),
+  mounted() {
+    window.addEventListener("resize", this.documentResize, {
+      passive: true
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.documentResize, {
+      passive: true
+    })
+  },
+  methods: {
+    documentResize() {
+      this.renderChart = window.innerWidth >= 768
+      this.renderRight = window.innerWidth >= 1224
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .index-component {
-	position: relative;
-	display: grid;
-	width: 100%;
-	@include mobile {
-		grid-template:
-			"boards" auto /
-			1fr;
-		grid-gap: 0rem;
-	}
+  position: relative;
+  display: grid;
+  width: 100%;
+  @include mobile {
+    grid-template:
+      "boards" auto /
+      1fr;
+    grid-gap: 0rem;
+  }
 
-	@include tablet {
-		grid-template:
-			"boards threads" auto
-			"boards threads" auto
-			"chart chart" auto /
-			1fr 1fr;
-		grid-gap: 0rem;
-		max-width: 100%;
-	}
+  @include tablet {
+    grid-template:
+      "boards threads" auto
+      "boards threads" auto
+      "chart chart" auto /
+      1fr 1fr;
+    grid-gap: 0rem;
+    max-width: 100%;
+  }
 
-	@include desktop {
-		grid-template:
-			"boards threads" auto
-			"boards threads" auto
-			"chart 	chart" auto /
-			1fr 1fr;
-		width: $fullhd;
-		grid-gap: 0rem;
-		max-width: calc(100%);
-	}
+  @include desktop {
+    grid-template:
+      "boards threads" auto
+      "boards threads" auto
+      "chart 	chart" auto /
+      1fr 1fr;
+    width: $fullhd;
+    grid-gap: 0rem;
+    max-width: calc(100%);
+  }
 
-	@include widescreen {
-		grid-template:
-			"boards threads right" auto
-			"boards threads right" auto
-			"chart 	chart 	right" auto /
-			1fr 1fr 0.5fr;
-		grid-gap: 1rem;
-		max-width: calc(100% - 2rem);
-		padding: 1rem 0 2rem;
-	}
+  @include widescreen {
+    grid-template:
+      "boards threads right" auto
+      "boards threads right" auto
+      "chart 	chart 	right" auto /
+      1fr 1fr 0.5fr;
+    grid-gap: 1rem;
+    max-width: calc(100% - 2rem);
+    padding: 1rem 0 2rem;
+  }
 }
 
 .boardlist-component {
-	grid-area: boards;
-	min-width: 100%;
+  grid-area: boards;
+  min-width: 100%;
 }
 
 .threadlist-component {
-	grid-area: threads;
-	min-width: 100%;
+  grid-area: threads;
+  min-width: 100%;
 }
 
 .chart-component {
-	grid-area: chart;
+  grid-area: chart;
 }
 
 .right {
-	grid-area: right;
-	min-width: 100%;
-	position: relative;
-	> .right-wrapper {
-		position: sticky;
-		top: 1rem;
-		> div:not(:first-child) {
-			margin-top: 1rem;
-		}
-	}
+  grid-area: right;
+  min-width: 100%;
+  position: relative;
+  > .right-wrapper {
+    position: sticky;
+    top: 1rem;
+    > div:not(:first-child) {
+      margin-top: 1rem;
+    }
+  }
 
-	> .really-makes-you-think-doesnt-it {
-		z-index: -1;
-		position: absolute;
-		bottom: -6rem;
-		right: -0rem;
-		width: 100%;
-		filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.25));
-	}
+  > .really-makes-you-think-doesnt-it {
+    z-index: -1;
+    position: absolute;
+    bottom: -6rem;
+    right: -0rem;
+    width: 100%;
+    filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.25));
+  }
 }
 </style>
